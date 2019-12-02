41f3b1881db79faf39cc21d69481366a42797799
zakizar <zakiyaainurrohman.zar@gmail.com>
2019-12-02 11:41:23 +0700

    update zaki

--- registration.php
+++ registration.php
@@ -1,11 +1,130 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<link href="registration.css" rel="stylesheet" type="text/css">
-<title>Untitled Document</title>
-</head>
+<?php require_once('Connections/koneksi.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
 
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO registrasi (nik, nama, email, alamat) VALUES (%s, %s, %s, %s)",
+                       GetSQLValueString($_POST['nik'], "text"),
+                       GetSQLValueString($_POST['nama'], "text"),
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['alamat'], "text"));
+
+  mysql_select_db($database_koneksi, $koneksi);
+  $Result1 = mysql_query($insertSQL, $koneksi) or die(mysql_error());
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<link href="index.css" rel="stylesheet" type="text/css">
+<head>	
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scalle=1.0">
+<meta name="author" content="Zakiya Ainur Rohman" >
+<meta name="description" content="ini adalah website booking kereta api">
+<title>KeretaBisa</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</head>
 <body>
+<div id="header">
+    	<img src="images/Logo.jpg" class="logo" alt="KitaBisa">
+        <div class="menu-atas">
+        	<ul>
+            	<li><a class="active" href="registration.php">Daftar</a></li>
+     			<li><a class="active" href="login.php">Masuk</a></li>
+   		    </ul>
+        </div>
+</div>
+    
+<div id="menu-samping">
+	<ul>
+    	<li><a href="#">Pemesanan</a></li>
+        <li><a href="#">Jadwal</a></li>
+        <li><a href="ticket.php">Riwayat</a></li>
+        <li><a href="#">Pengaturan</a></li>
+    </ul>
+</div>
+<div id="content">
+	<img src="images/kereta api.png" class="bg">
+    <div class="bs-example">
+	<div class="col-md-8 col-md-offset-2">
+	<div align="center" style="position:absolute;top:-800px;color:#FFF"><h2>Registrasi</h2></div>
+  <br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        
+    <form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+  <table align="center" bgcolor="#0099FF">
+  <div class="form-group">
+    <label for="noktp">No KTP :</label>
+    <input type="text" class="form-control" name="nik" value="" id="noktp" aria-describedby="emailHelp">
+  </div>
+
+  <div class="form-group">
+    <label for="nama">Nama :</label>
+    <input type="text" class="form-control" name="nama" value="" id="nama" aria-describedby="emailHelp">
+  </div>
+  
+  <div class="form-group">
+    <label for="email">Email :</label>
+    <input type="email" class="form-control" name="email" value="" id="email" aria-describedby="emailHelp">
+  </div>
+
+  <div class="form-group">
+    <label for="alamat">Alamat :</label>
+    <input type="text" class="form-control" name="alamat" value="" id="alamat" aria-describedby="emailHelp">
+  </div>
+
+  <button type="submit" value="Daftar" class="btn btn-primary">Daftar</button>
+
+  </table>
+  <input type="hidden" name="MM_insert" value="form1" />
+</form>
+     
+   		 </div>
+</div>
+</div>
+</div>
+<div id="footer">
+	<p style="color:#fff;" align="center">Copyright © 2019 KeretaBisa All rights reserved.</p>
+</div>
 </body>
 </html>
--- ticket.php
+++ ticket.php
@@ -1,48 +1,136 @@
-<!doctype html>
-<html>
-	<link href="index.css" rel="stylesheet" type="text/css">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scalle=1.0">
-		<meta name="author" content="Zakiya Ainur Rohman" >
-		<meta name="description" content="ini adalah website booking kereta api">
-		
-		<title>Kereta Bisa</title>
-	</head>
+<?php require_once('Connections/koneksi.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
 
-	<body>
-		<div id="header">
-			<img src="images/Logo.jpg" class="Logo" alt="Kereta Bisa">
-			<h1>Riwayat</h1>
-			<a href="login.html" class="logout">Logout</a>
-		</div>
-		<div id="content">
-			<div class="sidebar">
-				<nav>
-					<li><a href="Booking.html">Pemesanan</a></li>
-					<li><a href="Schedule.html">Jadwal</a></li>
-					<li><a href="ticket.html">Riwayat</a></li>
-					<li><a href="Setting.html">Pengaturan Akun</a></li>
-				</nav>
-			</div>
-			<table border=1>
-				<tr>
-					<th>Nama Kereta</th>
-					<th>Kelas Kereta</th>
-					<th>Tanggal Keberangkatan</th>
-					<th>Tanggal Kedatangan</th>
-					<th>Harga</th>
-					<th>Kode Pemesanan</th>
-				</tr>
-				<tr>
-					<td>Argo Wilis</td>
-					<td>Eksekutif</td>
-					<td>10 Desember 2019</td>
-					<td>11 Desember 2019</td>
-					<td>Rp 600.000</td>
-					<td>AR6BG</td>
-				</tr>
-		</div>
-	</body>
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$maxRows_Recordset1 = 10;
+$pageNum_Recordset1 = 0;
+if (isset($_GET['pageNum_Recordset1'])) {
+  $pageNum_Recordset1 = $_GET['pageNum_Recordset1'];
+}
+$startRow_Recordset1 = $pageNum_Recordset1 * $maxRows_Recordset1;
+
+mysql_select_db($database_koneksi, $koneksi);
+$query_Recordset1 = "SELECT * FROM riwayat_tiket";
+$query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
+$Recordset1 = mysql_query($query_limit_Recordset1, $koneksi) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+
+if (isset($_GET['totalRows_Recordset1'])) {
+  $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
+} else {
+  $all_Recordset1 = mysql_query($query_Recordset1);
+  $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
+}
+$totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<link href="index.css" rel="stylesheet" type="text/css">
+<head>	
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scalle=1.0">
+<meta name="author" content="Zakiya Ainur Rohman" >
+<meta name="description" content="ini adalah website booking kereta api">
+<title>KeretaBisa</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+<div id="header">
+    	<img src="images/Logo.jpg" class="logo" alt="KitaBisa">
+        <div class="menu-atas">
+        	<ul>
+            	<li><a class="active" href="registration.php">Daftar</a></li>
+     			<li><a class="active" href="login.php">Masuk</a></li>
+   		    </ul>
+        </div>
+</div>
+    
+<div id="menu-samping">
+	<ul>
+    	<li><a href="#">Pemesanan</a></li>
+        <li><a href="#">Jadwal</a></li>
+        <li><a href="ticket.php">Riwayat</a></li>
+        <li><a href="#">Pengaturan</a></li>
+    </ul>
+</div>
+<div id="content">
+	<img src="images/kereta api.png" class="bg">
+    <div class="bs-example">
+	<div class="col-md-8 col-md-offset-2">
+	<div align="center" style="position:absolute;top:-800px;color:#FFF"><h2>Riwayat</h2></div>
+  <br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+           
+    <table class="table table-bordered table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Kode Pemesanan</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Nama Kereta</th>
+      <th scope="col">Kelas Kereta</th>
+      <th scope="col">Tanggal Keberangkatan</th>
+      <th scope="col">Tanggal Kedatangan</th>
+      <th scope="col">Harga</th>
+      <th scope="col">Keterangan</th>
+    </tr>
+  </thead>
+          <?php do { ?>
+            <tr>
+              <td><?php echo $row_Recordset1['kode_pemesanan']; ?></td>
+              <td><?php echo $row_Recordset1['nama']; ?></td>
+              <td><?php echo $row_Recordset1['nama_kereta']; ?></td>
+              <td><?php echo $row_Recordset1['kelas_kereta']; ?></td>
+              <td><?php echo $row_Recordset1['tgl_keberangkatan']; ?></td>
+              <td><?php echo $row_Recordset1['tgl_kedatangan']; ?></td>
+              <td><?php echo $row_Recordset1['harga']; ?></td>
+              <td><?php echo $row_Recordset1['ket']; ?></td>
+            </tr>
+            <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
+        </table>
+     
+   		 </div>
+</div>
+</div>
+</div>
+<div id="footer">
+	<p style="color:#fff;" align="center">Copyright © 2019 KeretaBisa All rights reserved.</p>
+</div>
+</body>
 </html>
